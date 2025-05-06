4e907f6d28d961b3189513376171773e775add37
JehanPinto <judepinto73@gmail.com>
2025-05-06 11:07:01 +0530

    up

--- src/app/staticPages/AboutUs/page.tsx
+++ src/app/staticPages/AboutUs/page.tsx
@@ -1,10 +1,4 @@
-import { ReactNode } from "react";
-
-interface StaticLayoutProps {
-  children: ReactNode;
-}
-
-export default function ContactUs({ children }: StaticLayoutProps) {
+export default function ContactUs() {
   return (
     <div className="min-h-screen flex flex-col items-center py-0 rounded-2xl shadow-lg bg-gradient-to-r from-[#E1F3FD] to-[#FFFFFF] ">
       <h1 className="text-[75px] font-bold bg-gradient-to-r from-[#1045A1] to-[#527cc4] 
@@ -13,28 +7,24 @@ export default function ContactUs({ children }: StaticLayoutProps) {
       </h1>
       <h3 className="text-3xl font-bold text-black p-5">Empowering Minds, Connecting Hearts</h3>
       <div className="mt-5  p-6 rounded-lg">
-            <p className="text-[18px] pb-9 text-gray-900 leading-relaxed">
-              At <strong>Mindful Connect</strong>, we believe that mental well-being should be 
-              accessible, secure, and stigma-free. Our platform bridges the gap between 
-              individuals seeking support and professional counselors, fostering a 
-              compassionate space for healing and growth.
-            </p>
-
-            <p className="text-[18px] text-gray-900 leading-relaxed mt-4 pb-9">
-              Driven by innovation, we integrate cutting-edge technology with 
-              human-centered care. From secure messaging to insightful progress tracking, 
-              every feature is designed to empower users on their mental health journey.
-            </p>
-
-            <p className="text-[18px] text-gray-900 leading-relaxed mt-4">
-              More than just a platform, <strong>Mindful Connect</strong> is a movement—a 
-              commitment to reshaping the future of mental health support. Together, we 
-              are building a world where emotional wellness is not just prioritized 
-              but truly embraced.
-            </p>
-          </div>
-      
-      
+        <p className="text-[18px] pb-9 text-gray-900 leading-relaxed">
+          At <strong>Mindful Connect</strong>, we believe that mental well-being should be 
+          accessible, secure, and stigma-free. Our platform bridges the gap between 
+          individuals seeking support and professional counselors, fostering a 
+          compassionate space for healing and growth.
+        </p>
+        <p className="text-[18px] text-gray-900 leading-relaxed mt-4 pb-9">
+          Driven by innovation, we integrate cutting-edge technology with 
+          human-centered care. From secure messaging to insightful progress tracking, 
+          every feature is designed to empower users on their mental health journey.
+        </p>
+        <p className="text-[18px] text-gray-900 leading-relaxed mt-4">
+          More than just a platform, <strong>Mindful Connect</strong> is a movement—a 
+          commitment to reshaping the future of mental health support. Together, we 
+          are building a world where emotional wellness is not just prioritized 
+          but truly embraced.
+        </p>
+      </div>
     </div>
   );
 }
